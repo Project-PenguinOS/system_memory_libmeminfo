1ea1cb6477a275db75062d690778417c76101683
Pawan Wagh <waghpawan@google.com>
2025-09-09 23:01:04 +0000

    Add API in sysmeminfo to parse size strings
    Adding parsing utility to get the memory from size string
    like ro.boot.ddr_size.
    
    Test: atest libmeminfo_test
    Bug: 441549522
    Test: atest Vts16KPageSizeTest
    Flag: EXEMPT vts test
    Change-Id: I16118e612351423bd78423e482d07cb38a87d432

--- sysmeminfo.cpp
+++ sysmeminfo.cpp
@@ -32,6 +32,7 @@
 #if defined(__ANDROID__) && !defined(__ANDROID_APEX__) && !defined(__ANDROID_VNDK__)
 #include "bpf/BpfMap.h"
 #endif
+#include <regex>
 #include <sstream>
 #include <string>
 #include <unordered_set>
@@ -517,6 +518,42 @@ bool ReadKernelCmaUsageKb(uint64_t* size, const std::string& cma_stats_sysfs_pat
     }
 
     return true;
+}
+
+std::optional<uint64_t> ParseSizeToBytes(const std::string& str) {
+    if (str.empty()) {
+        return std::nullopt;
+    }
+
+    static const std::regex sizeRegex(R"(\s*(0|[1-9][0-9]*)\s*(B|[KMG](?:i?B)?)\s*)",
+                                      std::regex::icase);
+    std::smatch match;
+
+    if (std::regex_match(str, match, sizeRegex)) {
+        uint64_t value;
+        if (android::base::ParseUint(match[1].str(), &value)) {
+            std::string unitStr = match[2].str();
+            std::transform(unitStr.begin(), unitStr.end(), unitStr.begin(), ::toupper);
+
+            if (unitStr == "GIB" || unitStr == "GI") {
+                return value * 1024 * 1024 * 1024;
+            } else if (unitStr == "GB" || unitStr == "G") {
+                return value * 1000 * 1000 * 1000;
+            } else if (unitStr == "MIB" || unitStr == "MI") {
+                return value * 1024 * 1024;
+            } else if (unitStr == "MB" || unitStr == "M") {
+                return value * 1000 * 1000;
+            } else if (unitStr == "KIB" || unitStr == "KI") {
+                return value * 1024;
+            } else if (unitStr == "KB" || unitStr == "K") {
+                return value * 1000;
+            } else if (unitStr == "B") {
+                return value;
+            }
+        }
+    }
+
+    return std::nullopt;
 }
 
 }  // namespace meminfo
